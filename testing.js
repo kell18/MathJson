03b74829eae33411211177ad189494bef6b59c8e
bk0606 <albert.bikeev@gmail.com>
2013-10-18 13:47:04 +0400

    Rewrite testing.js and testing.html
    Add ability to parse with GET request and add bootstrap import in html
    document.

--- testing.js
+++ testing.js
@@ -11,8 +11,26 @@ window.onload = function () {
 		if(ME == null)
 			alert("Incorrect expression");
 		else
-			JSONBlock.innerHTML = ME.getHtml();
+			JSONBlock.innerHTML = ME.getJSONHtml();
 	};
+	
+	var get = location.search;
+	if(get !== ""){
+		var tmp1 = (get.substr(1)).split('&');	// разделяем переменные
+		var tmp2 = [];
+		var param = {};
+		for(var i=0; i < tmp1.length; i++) {
+			tmp2 = tmp1[i].split('=');		// массив param будет содержать
+			param[tmp2[0]] = tmp2[1];		// пары ключ(имя переменной)->значение
+		}
+		JSONBlock = document.getElementById("json-block");
+		mathInp.value = param.expression;
+		var ME = new MathExpression(param.expression);
+		if(!ME)
+			alert("Incorrect expression");
+		else
+			JSONBlock.innerHTML = ME.getJSONHtml();
+	}
 		
 }
 	
